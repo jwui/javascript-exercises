759af92248f0dfcae62da3688aad0f1822ac8b0f
snoopy2080 <49978401+snoopy2080@users.noreply.github.com>
2022-05-02 15:35:31 +0900

    Finished 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,11 @@
-const sumAll = function() {
-
+var sum = 0;
+const sumAll = function (num1, num2) {
+  for (let i = num1; i < num2 + 1; i++) {
+    sum += i;
+  }
+  console.log(sum);
 };
 
+sumAll(1, 5);
 // Do not edit below this line
 module.exports = sumAll;
